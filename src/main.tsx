e83fb86957d32b1622166d6b267c3debb1d5e23d
Luan Correa <luanmcor@gmail.com>
2026-04-20 09:53:08 -0300

    fix clarity

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./styles/index.css"
+import "./utils/clarity.js"
 import App from "./App.tsx"
 import { BrandThemeProvider } from "@/components/brand-theme-provider.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
--- src/utils/clarity.js
+++ src/utils/clarity.js
@@ -1,5 +1,30 @@
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "wdc0n9r5tf");
+const CLARITY_PROJECT_ID = "wdc0n9r5tf"
+
+function initializeClarity() {
+  if (typeof window === "undefined" || typeof document === "undefined") {
+    return
+  }
+
+  if (window.__clarityInitialized) {
+    return
+  }
+
+  ;(function (c, l, a, r, i, t, y) {
+    c[a] =
+      c[a] ||
+      function () {
+        ;(c[a].q = c[a].q || []).push(arguments)
+      }
+    t = l.createElement(r)
+    t.async = 1
+    t.src = "https://www.clarity.ms/tag/" + i
+    y = l.getElementsByTagName(r)[0]
+    y.parentNode.insertBefore(t, y)
+  })(window, document, "clarity", "script", CLARITY_PROJECT_ID)
+
+  window.__clarityInitialized = true
+}
+
+initializeClarity()
+
+export {}
