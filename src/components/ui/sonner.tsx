6da1be4b616561f911b25b27d92404e2230145f3
Luan Correa <luanmcor@gmail.com>
2026-04-20 16:08:08 -0300

    update: icon weight="fill" in sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from "@/components/theme-provider"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
-import { CheckCircleIcon, InfoIcon, WarningIcon, XCircleIcon } from "@phosphor-icons/react"
+import { CheckCircleIcon, InfoIcon, WarningCircleIcon, XCircleIcon } from "@phosphor-icons/react"
 import { Spinner } from "@/components/ui/spinner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
@@ -12,16 +12,16 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       icons={{
         success: (
-          <CheckCircleIcon className="size-4" />
+          <CheckCircleIcon weight="fill" className="size-4" />
         ),
         info: (
-          <InfoIcon className="size-4" />
+          <InfoIcon weight="fill" className="size-4" />
         ),
         warning: (
-          <WarningIcon className="size-4" />
+          <WarningCircleIcon weight="fill" className="size-4" />
         ),
         error: (
-          <XCircleIcon className="size-4" />
+          <XCircleIcon weight="fill" className="size-4" />
         ),
         loading: (
           <Spinner className="size-4" />
